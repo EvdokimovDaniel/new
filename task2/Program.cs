bb3b0bf18a6f2c660f0ef8f42b7c43add12bec1b
Daniel <evdokimov.d.1308@gmail.com>
2024-11-22 19:29:52 +0300

    new commit

--- task2/Program.cs
+++ task2/Program.cs
@@ -4,11 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-       int x = 0;
-       if (x > 0)
+       int x = 1;
+       if (x > 5)
        {
-           Console.WriteLine("x больше нулю");
+           Console.WriteLine("x больше 5");
        }
+        else if (x > 0)
+        {
+            Console.WriteLine("x больше нуля");
+        }
         else if (x < 0)
         {
             Console.WriteLine("x меньше нуля");
