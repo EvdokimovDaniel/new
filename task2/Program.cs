545f3d9a4dc98b5df29478da84e5815f62271281
Daniel <evdokimov.d.1308@gmail.com>
2024-11-22 19:28:57 +0300

    first commit

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       int x = 0;
+       if (x > 0)
+       {
+           Console.WriteLine("x больше нулю");
+       }
+        else if (x < 0)
+        {
+            Console.WriteLine("x меньше нуля");
+        }
+        else
+       {
+          Console.WriteLine("x равен нулю");
+       }
+    }
+}
